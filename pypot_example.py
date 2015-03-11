03bb7a8926fde16ad777c6124bac8b251e796eb1
corentinCharles <charles.c1@laposte.net>
2015-03-11 18:28:34 +0100

    adding the robot file and ths json file needed to create a robot object

--- pypot_example.py
+++ pypot_example.py
@@ -11,35 +11,59 @@ def change_motorid(found_ids,dxl_io):
     #found_ids = dxl_io.scan()#   this may take several seconds
     # It is essential to know the current labels to prevent from making errors
 	print 'Detected:', found_ids
-    
+	i=0
+	# dxl_io.switch_led_on(found_ids)
+
+
+	
     # This loop goes through all the motors and asks the user to relabel
-	for motor_id in found_ids:
-		print "When labelling the motors please do not use any of the already detected ones as it will produce an error!"
+	# for id in found_ids:
+
+	# 	i+=1
+	# 	print "When labelling the motors please do not use any of the already detected ones as it will produce an error!"
 		# Lights up the motor 
-		print "********"
-		print type(motor_id)
-		print "********"
-		dxl_io.switch_led_on(motor_id)
+		# print i
+		# print type([id])
+		#print "********"
+		# dxl_io.switch_led_on([id])
+		# input("Look everywhere!")
+		# dxl_io.switch_led_off([id])
+
 		# I think it is only possible to use integers but I can't find any    # documentation
-		new_id = int(input("Please enter an integer to label the lit motor"))
-		dxl_io.change_id({motor_id : new_id})
+		# new_id = int(input("Please enter an integer to label the lit motor"))
+		# dxl_io.change_id([new_id])
 	# To check whether everything went according to plan    
 	found_ids = dxl_io.scan()
 	print "The new id's are: ", found_ids
 
+def move_leg(id,dxl_io):
+	# expected_position = (25.66, -6.01, 12.17, -26.54, -1.61, 22.14, 44.43, -5.43, 19.21, 1.61, -8.36, -3.08, -37.1, -3.37, -18.91, 37.39, -1.61, 8.65)
+
+	# we power on the motors
+	dxl_io.enable_torque(found_ids)
+	# print 'Current pos:', dxl_io.get_present_position(found_ids)
+	pos = {id[9] : 220,id[10] : leg_ik(0,0,0)[1],id[11] : leg_ik(0,0,0)[2]}
+	# This moves it to the set position
+	dxl_io.set_goal_position(pos)
+	time.sleep(1)
+	
+	dxl_io.disable_torque(found_ids)
+
+
 def leg_ik(x3,y3,z3,alpha = 20.69, beta = 5.06,l1=51,l2=63.7,l3=93):
 	d13 = math.sqrt(x3*x3 + y3*y3) - l1
 	d = math.sqrt(d13*d13 + z3*z3)
-	tmp = (l2*l2 + d*d - l3*l3)/(2*l2*d)
+	tmp = (l2**2
+	 + d**2 - l3**2)/(2*l2*d)
 	a1 = z3 / d13
-	a2 = (l2*l2 + l3*l3 - d*d)/(2*l2*l3)
+	a2 = (l2**2 + l3**2 - d**2)/(2*l2*l3)
 	#~ print a2
 	#~ print a1
 	#~ print tmp
 	angles = (None,None,None)
-	theta1 = math.degrees(math.atan2(y3,x3))
-	theta2 = math.degrees(math.atan(a1) + math.acos(tmp))
-	theta3 = 180 - math.degrees(math.acos(a2))
+	theta1 = math.degrees(math.atan2(y3,x3))   # OK
+	theta2 = math.degrees(math.atan(a1) + math.acos(tmp)) 
+	theta3 = 180 + math.degrees(math.acos(a2))
 	# Corrections to the angles theta2 and theta3
 	theta2 = theta2 + alpha
 	theta3 = -(theta3 - 90 + alpha + beta)
@@ -48,30 +72,35 @@ def leg_ik(x3,y3,z3,alpha = 20.69, beta = 5.06,l1=51,l2=63.7,l3=93):
   
 if __name__ == '__main__':
     # we first open the Dynamixel serial port
+	# robot = pypot.dynamixel.autodetect_robot()
 	with pypot.dynamixel.DxlIO('/dev/ttyUSB0', baudrate=1000000) as dxl_io:   
-		found_ids = [1, 2, 3]#dxl_io.scan()
-		print "-----------------------------"
-		print found_ids
-		print "-----------------------------"
-		#change_motorid(found_ids,dxl_io)
-		# we power on the motors
-		dxl_io.enable_torque(found_ids)
+		found_ids = [11, 12, 13, 21, 22, 23, 31, 32, 33, 41, 42, 43, 51, 52, 53, 61, 62, 63]
+		# print "-----------------------------"
+		# print found_ids
+		# print "-----------------------------"
+		# dxl_io.switch_led_off(found_ids)
+		# found_ids = dxl_io.scan()
+		# print found_ids
+		# change_motorid(found_ids,dxl_io)
 		# we get the current positions
-		print 'Current pos:', dxl_io.get_present_position(found_ids)
-		pos = {found_ids[0] : 0,found_ids[1] : 0,found_ids[2] : 0}
-		#print 'Cmd:', pos
+		# print 'Current pos:', dxl_io.get_present_position(found_ids)
+		# pos = {found_ids[0] : 0,found_ids[1] : 0,found_ids[2] : 0}
+		# print pos
+		# print 'Cmd:', pos
 		#~ T = time.time()
 		# This moves it to the set position
-		dxl_io.set_goal_position(pos)
-		time.sleep(1)
-
-
+		# dxl_io.set_goal_position(pos)
+		# time.sleep(1)
 		position = dxl_io.get_present_position(found_ids)
+		print position
+		move_leg(found_ids,dxl_io)
+		position = dxl_io.get_present_position(found_ids)
+		
 		# Angles that correspond to current position
-		print leg_ik(position[0],position[1],position[2])
+		# print leg_ik(position[0],position[1],position[2])
 		# we get the current positions
 		print 'New pos:', dxl_io.get_present_position(found_ids)
 		# we power off the motors
-		dxl_io.disable_torque(found_ids)
+		# dxl_io.disable_torque(found_ids)
 		
-		time.sleep(1)  # we wait for 1s
+		# time.sleep(1)  # we wait for 1s
---
+++ robot.py
@@ -0,0 +1,134 @@
+import itertools
+import time
+import numpy
+from pypot.dynamixel import autodetect_robot
+import pypot.dynamixel
+import math
+import json
+import time
+from contextlib import closing
+
+import pypot.robot
+# from pypot.robot.config import "my_robot.json"
+
+asterix = None
+
+
+def leg_ik(x3,y3,z3,alpha = 20.69, beta = 5.06,l1=51,l2=63.7,l3=93):
+	d13 = math.sqrt(x3*x3 + y3*y3) - l1
+	d = math.sqrt(d13*d13 + z3*z3)
+	tmp = (l2**2 + d**2 - l3**2)/(2*l2*d)
+	a1 = z3 / d13
+	a2 = (l2**2 + l3**2 - d**2)/(2*l2*l3)
+	
+	angles = (None,None,None)
+	theta1 = math.degrees(math.atan2(y3,x3))   # OK
+
+	theta2 = math.degrees(math.atan(a1) + math.acos(tmp)) 
+	theta3 = 180 - math.degrees(math.acos(a2))
+	# Corrections to the angles theta2 and theta3
+	theta2 = -(theta2 + alpha)
+	theta3 = -(theta3 - 90 + alpha + beta)
+	angles = (theta1,theta2,theta3)
+	return angles 
+
+def detection():
+
+	my_robot = autodetect_robot()
+
+	config = my_robot.to_config()
+
+	with open('my_robot.json', 'wb') as f:
+	    json.dump(config, f)
+
+	with closing(pypot.robot.from_json('my_robot.json')) as my_robot:
+	    # do stuff without having to make sure not to forget to close my_robot!
+	    pass
+
+
+def initialize():
+
+	asterix = pypot.robot.from_json('my_robot.json')
+	# print asterix
+	# Note that all these calls will return immediately,
+	# and the orders will not be directly sent
+	# (they will be sent during the next write loop iteration).
+	for m in asterix.motors:
+	    print m.present_position
+	    m.compliant = False
+	    m.goal_position = 0
+
+
+	time.sleep(2)
+	return asterix
+
+	# This will return the last synchronized value
+
+def motion(asterix):
+	# Do the sinusoidal motions for 10 seconds
+	amp = 30
+	freq = 0.5
+	t0 = time.time()
+
+	while True:
+		t = time.time() - t0
+
+		if t > 10:
+			break
+
+		pos = amp * numpy.sin(2 * numpy.pi * freq * t)
+
+		asterix.motor_51.goal_position = pos
+
+	    # In order to make the other sinus more visible,
+	    # we apply it with an opposite phase and we increase the amplitude.
+		asterix.motor_41.goal_position = -1.5 * asterix.motor_51.present_position
+
+	    # We want to run this loop at 50Hz.
+		time.sleep(0.02)
+
+def move_leg(x,y,z,leg):
+	i=0
+	tupl = leg_ik(x,y,z)
+	for m in leg:
+		time.sleep(0.1)
+		m.goal_position = tupl[i]
+		i+=1
+
+
+def experimentation(asterix):
+	tupl = leg_ik(100,0,0)
+	asterix.motor_41.goal_position = tupl[0]
+	asterix.motor_42.goal_position = tupl[1]
+	asterix.motor_43.goal_position = tupl[2]
+	time.sleep(0.5)
+	tupl = leg_ik(100,-50,-110)
+	asterix.motor_41.goal_position = tupl[0]
+	asterix.motor_42.goal_position = tupl[1]
+	asterix.motor_43.goal_position = tupl[2]
+	# indirect_kinematics(0,50,0,Constants) = (theta1,theta2,theta3)
+	# asterix.motor_41.change_angle = theta1
+	# asterix.motor_42.change_angle = theta2
+	# asterix.motor_43.change_angle = theta3
+	# asterix.motor_43.goal_position = 0
+	asterix.close()
+
+# def move_couple_leg(x,y,z)
+
+# def move_all(x,y,z):
+
+# 	legs = [obj.leg1,obj.leg2,obj.leg6,obj.leg4,obj.leg3,obj.leg5]
+
+# 	move_leg(x,y,z,legs[0])
+# detection()
+obj = initialize()
+# experimentation(obj)
+# motion(obj)
+legs = [obj.leg1,obj.leg2,obj.leg6,obj.leg4,obj.leg3,obj.leg5]
+
+move_leg(100,50,-110,legs[0])
+move_leg(100,-50,-110,legs[1])
+move_leg(100,50,-110,legs[2])
+move_leg(100,50,-110,legs[3])
+move_leg(100,50,-110,legs[4])
+move_leg(100,-50,-110,legs[5])
