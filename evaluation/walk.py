f49be2317d5b8553dc3cac64b48885436bbd60b1
corentinCharles <charles.c1@laposte.net>
2015-04-02 18:02:24 +0200

    changes, comments but events not working yet

--- evaluation/walk.py
+++ evaluation/walk.py
@@ -44,7 +44,7 @@ def leg_ik(x3,y3,z3,alpha = 20.69, beta = 5.06,l1=51,l2=63.7,l3=93):
         theta3 = -(theta3 - 90 + alpha + beta)
         angles = (theta1,theta2,theta3)
     except ValueError:
-        print "The legs of the robot cannot go that far!!"
+        print "The leg of the robot cannot go that far!!"
         
     return angles
 
@@ -57,14 +57,13 @@ def get_legs(obj):
 """
 	Makes one leg move.
 	parameters:
-		- L : The length between the start point and the end point (in a right line)
+		- L : The length between the start point and the end point of each leg (in a straight line).
 		- z : THe height of the center of the robot.
 		- leg : The leg we want to move
 """
 def move_leg(L,z,leg):
 	num = int(leg[0].id*0.1)-1
-	print initial
-	theta = math.atan(initial[num][1]/initial[num][0])	#intial[number of the leg][number of the motor]
+	theta = math.atan(initial[num][1]/initial[num][0])
 	hypo = math.sqrt(initial[num][0]**2 + initial[num][1]**2)
 	x = math.cos(theta)*(hypo+L)
 	y = math.sin(theta)*(hypo+L)
@@ -76,58 +75,78 @@ def move_leg(L,z,leg):
 		i+=1
 
 """"
-Make the robot move along his two separate legs
+	Make the robot move along his two separate legs
+	Parameters:
+		- L : The length between the start point and the end point of each leg (in a straight line).
+		- z : THe height of the center of the robot.
 """
-def move_center_forward(L,z):
+def move_center_forward(L,z,max_length=50):
+	print "move_center_forward({0},{1},...)".format(L,z)
+
 	break_length = 2
 	theta = 20	#more than 20 would make the legs touch for a sec (because of the speed)
 	order = [1,5,2,4]
+
+	if L>max_length:
+		move_center_forward(L-max_length,z)
+		L = L -max_length
+	
+	initial = rotation.initial_pos(30,z)
 	if L<0:
 		order = [4,2,5,1]
-	rotation.initial_pos(0,-60)
 	move_leg(L,z+40,legs[0])
+	print "move_leg({0},{1},...)".format(L,z)
 	move_leg(-L,z+40,legs[3])
+	print "here 3"
 	time.sleep(break_length)
 
 	move_leg(L,z,legs[0])
 	move_leg(-L,z,legs[3])
 	time.sleep(break_length)
-	for i in order:
-		if i==order[0] or i==order[2]:
-			rotation.move_leg(-theta,z+40,legs[i])
-		else:
-			rotation.move_leg(theta,z+40,legs[i])
-			time.sleep(break_length)
-	for i in order:
-		if i==order[0] or i==order[2]:
-			rotation.move_leg(-theta,z,legs[i])
-		else:
-			rotation.move_leg(theta,z,legs[i])
-			time.sleep(break_length)
+
+	rotation.move_leg(-theta,z+40,legs[order[0]])
+	rotation.move_leg(theta,z+40,legs[order[1]])
+	time.sleep(break_length)
+	rotation.move_leg(-theta,z,legs[order[0]])
+	rotation.move_leg(theta,z,legs[order[1]])
+	time.sleep(break_length)
+
+	rotation.move_leg(-theta,z+40,legs[order[2]])
+	rotation.move_leg(theta,z+40,legs[order[3]])
+	time.sleep(break_length)
+	rotation.move_leg(-theta,z,legs[order[2]])
+	rotation.move_leg(theta,z,legs[order[3]])
 	time.sleep(break_length)
 
 """
 THEORICAL WORK: The order of the leg or the direction could be wrong...TO TEST
 Make the robot move along its two legged side.
 """
-def move_center_aside(L,z):
-
-	break_length = 1.2
+def move_center_aside(L,z,max_length=50):
+	print "move_center_aside"
+	print "move_center_aside({0},{1},...)".format(L,z)
+	break_length = 1
 	theta = 20
+
+	if L>max_length:
+		move_center_aside(L-max_length,z)
+		L = L -max_length
+
 	if L<0:
 		theta = -theta
 
-	initial = rotation.initial_pos(30,-60)
+	initial = rotation.initial_pos(30,z)
 	time.sleep(break_length)
-	print "putting legs 2-3-5-6 in the air"
+	print "putting legs 2-3 in the air"
+	print "move_leg({0},{1},...)".format(L,z+40)
 	move_leg(L,z+40,legs[1])
 	move_leg(L,z+40,legs[2])
 	time.sleep(break_length)
 	move_leg(L,z,legs[1])
 	move_leg(L,z,legs[2])
-		
+	time.sleep(break_length)	
 	
-	print "putting legs 2-3-5-6 on the ground"
+	print "putting legs 5-6 on the ground"
 	move_leg(-L,z+40,legs[4])
 	move_leg(-L,z+40,legs[5])
 	time.sleep(break_length)
@@ -144,38 +163,14 @@ def move_center_aside(L,z):
 	rotation.move_leg(-theta,z,legs[3])
 	time.sleep(break_length)
 #move the center with leg 2, 4, 6 in the air	
-def pos_in_air(theta,z):
-	initial_position = []
-	initial_position.append(move_leg(0,z,legs[0]))
-	initial_position.append(move_leg(-abs(theta),z+40,legs[1]))
-	initial_position.append(move_leg(abs(theta),z,legs[2]))
-	initial_position.append(move_leg(0,z+40,legs[3]))
-	initial_position.append(move_leg(-abs(theta),z,legs[4]))
-	initial_position.append(move_leg(abs(theta),z+40,legs[5]))
-	time.sleep(0.1)
-	return initial_position
-#ne donne pas une bonne marche du tout	
-def move_center_with_panache(L,z):
-	break_length = 1
-	theta = 20 
-	initial = rotation.initial_pos(30,-60)	
-	#1, 3, 5 in the air to the new pos
-	move_leg(L,z+40,legs[2])
-	move_leg(-L,z+40,legs[4])
-	rotation.move_leg(theta,z+40,legs[0])
-	time.sleep(break_length)
-	#1,3,5 on the ground to the new pos
-	move_leg(L,z,legs[2])
-	move_leg(-L,z,legs[4])
-	rotation.move_leg(theta,z,legs[0])	
-	time.sleep(break_length)
-	#2,4,6 in the air	
-	pos_in_air(30,-60)
-	time.sleep(break_length)
-	#go back to the initial position
-	initial
-	time.sleep(break_length)
-	
+
+
+"""
+	Move all the leg in their own coordonnate system. 
+	Parameters:
+		- L : The length between the start point and the end point of each leg (in a straight line).
+		- z : THe height of the center of the robot.
+"""	
 def moving_all_legs(L,z):
 	move_leg(L,z,legs[0])
 	move_leg(L,z,legs[1])
